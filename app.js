dcb9744a9b8202b66affb2874fd3882b1bdfb0de
Shen GuoQing <yinweiyingzhe@126.com>
2023-07-05 14:53:07 +0800

    显示初始化数据

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
+import { showMsg } from "./utils";
 
 App({
   onLaunch(options) {
     // 第一次打开
     // options.query == {number:1}
     console.info('App onLaunch');
+    my.alert({
+      content: 'onLaunch : ' + JSON.stringify(options),
+    })
+    showMsg(JSON.stringify(options), undefined, {
+      duration: 30000
+    })
   },
   onShow(options) {
     // 从后台被 scheme 重新打开
     // options.query == {number:1}
+    my.alert({
+      content: 'onShow : ' + JSON.stringify(options),
+    })
+    showMsg(JSON.stringify(options), undefined, {
+      duration: 30000
+    })
   }
 });
