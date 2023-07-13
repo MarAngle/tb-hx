3c922d99ed1d0345b181e581f121fee23390bfd3
MarAngle <yinweiyingzhe@126.com>
2023-07-13 21:49:12 +0800

    专区对接，样式调优

--- data/category.js
+++ data/category.js
@@ -6,17 +6,17 @@ class CategoryData extends BaseData{
     super(initOption)
     this.data = {
       left: {
-        name: '衣鞋洗护专区',
+        name: '衣鞋洗护服务',
         id: 1,
         icon: ''
       },
       rightTop: {
-        name: '家纺专区',
+        name: '家纺洗护服务',
         id: 2,
         icon: ''
       },
       rightBottom: {
-        name: '奢护专区',
+        name: '奢品养护',
         id: 3,
         icon: ''
       }
@@ -28,19 +28,33 @@ const category = new CategoryData({
   prop: 'category',
   getData() {
     return new Promise((resolve, reject) => {
-      // require.get({
-      //   url: 'https://ihuanxi.cn',
-      //   headers: {},
-      //   data: {},
-      //   timeout: 0,
-      //   dataType: '',
-      //   token: false
-      // }).then(res => {
-      //   resolve(res)
-      // }).catch(err => {
-      //   reject(err)
-      // })
-      resolve()
+      require.post({
+        url: '/tb_api/api/TradeItem.php',
+        data: {
+          status: 'showZone'
+        },
+        timeout: 0,
+        token: true
+      }).then(res => {
+        for (let i = 0; i < res.data.length; i++) {
+          const oitem = res.data[i];
+          if (oitem.zone_id == 1) {
+            this.data.left.name = oitem.zone_name
+            this.data.left.icon = oitem.zone_picture
+          } else if (oitem.zone_id == 2) {
+            this.data.rightTop.name = oitem.zone_name
+            this.data.rightTop.icon = oitem.zone_picture
+          } else if (oitem.zone_id == 3) {
+            this.data.rightBottom.name = oitem.zone_name
+            this.data.rightBottom.icon = oitem.zone_picture
+          }
+        }
+        this.$syncPage()
+        resolve(res)
+      }).catch(err => {
+        console.error(err)
+        reject(err)
+      })
     })
   }
 })
