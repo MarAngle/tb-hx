20a93eb0885977526fbc8a3d3afdea894aec98dc
Shen GuoQing <yinweiyingzhe@126.com>
2023-07-19 11:26:12 +0800

    优化首页

--- pages/home/index.js
+++ pages/home/index.js
@@ -3,12 +3,34 @@ import productList from "../../data/productList";
 import category from "./../../data/category";
 import { createLifePage } from "../../utils/page";
 
+// fresourceniche id: "1", resourceniche na
+// 1: {resourceniche id: "2", resourceniche na
+// 2: [resourceniche id: "3", resourceniche na
+// success: true
+// "热销
+// ame:
+// 0i
+// 0i
+// e:
+// "热销”
+// "上新"
+// "更多推若
+
 Page(createLifePage({
   data: {
-    list: []
+    list: [],
+    resourceniche: '1'
   },
   createList() {
     this.data.list = [...productList.list]
+    let list = []
+    for (let i = 0; i < productList.list.length; i++) {
+      const item = productList.list[i];
+      if (item.commodity_resourceniche_id == this.data.resourceniche) {
+        list.push(item)
+      }
+    }
+    this.data.list = list
     this.setData({
       list: this.data.list
     })
@@ -23,6 +45,16 @@ Page(createLifePage({
     my.navigateTo({
       url: `/pages/product/detail/index?id=${productId}`
     })
+  },
+  changeProduct({ target }) {
+    const id = target.dataset.id
+    if (this.data.resourceniche != id) {
+      this.data.resourceniche = id
+      this.createList()
+      this.setData({
+        resourceniche: this.data.resourceniche
+      })
+    }
   }
 }, {
   load() {
