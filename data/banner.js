adf5d2ca1610f9a969e120d3a5ead5f670b43c5e
MarAngle <yinweiyingzhe@126.com>
2023-07-20 21:47:20 +0800

    添加banner

--- data/banner.js
+++ data/banner.js
@@ -1,5 +1,5 @@
 import BaseData from "../class/BaseData";
-import requireS from "../utils/require";
+import require from "../utils/require";
 
 class BannerData extends BaseData{
   constructor(initOption) {
@@ -12,38 +12,21 @@ const banner = new BannerData({
   prop: 'banner',
   getData() {
     return new Promise((resolve, reject) => {
-      // require.get({
-      //   url: 'https://ihuanxi.cn',
-      //   headers: {},
-      //   data: {},
-      //   timeout: 0,
-      //   dataType: '',
-      //   token: false
-      // }).then(res => {
-      //   resolve(res)
-      // }).catch(err => {
-      //   reject(err)
-      // })
-      setTimeout(() => {
-        this.list = [
-          {
-            id: 1,
-            product: 300000000028,
-            icon: require('/image/test/banner1.jpg')
-          },
-          {
-            id: 2,
-            product: 300000000026,
-            icon: require('/image/test/banner2.jpg')
-          },
-          {
-            id: 3,
-            product: 300000000036,
-            icon: require('/image/test/banner3.jpg')
-          },
-        ]
-        resolve()
-      }, 0)
+      require.post({
+        url: '/tb_api/api/TradeItem.php',
+        data: {
+          status: 'showSwiper'
+        },
+        timeout: 0,
+        token: false
+      }).then(res => {
+        let originList = res.data || []
+        this.list = originList
+        resolve(res)
+      }).catch(err => {
+        console.error(err)
+        reject(err)
+      })
     })
   }
 })
